66f900d7e5dcb293df38a8a3ceea25f123f910fb
Ruslan <krutiporohruslan@gmail.com>
2023-04-26 12:00:40 +0400

    New packages

--- src/main/java/mytaxi/MyTaxiApplication.java
+++ src/main/java/mytaxi/MyTaxiApplication.java
@@ -1,11 +1,10 @@
-package com.partola.mytaxi;
+package mytaxi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyTaxiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MyTaxiApplication.class, args);
         System.out.println("test");
