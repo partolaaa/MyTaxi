0143f43a4346dfae709f2ce4ac815ad3ae4edec5
partolaaa <ivan.partola77@gmail.com>
2023-04-26 12:41:26 +0200

    Test with Ruslan

--- src/main/java/com/partola/mytaxi/MyTaxiApplication.java
+++ src/main/java/com/partola/mytaxi/MyTaxiApplication.java
@@ -8,7 +8,7 @@ public class MyTaxiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyTaxiApplication.class, args);
-        System.out.println("asdadasd");
+        System.out.println("test");
     }
 
 }
