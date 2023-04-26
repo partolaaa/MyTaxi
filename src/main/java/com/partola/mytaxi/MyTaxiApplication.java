f623506c497ac27992a23981bfc623d70a658273
partolaaa <ivan.partola77@gmail.com>
2023-04-26 12:53:52 +0200

    Test 2

--- src/main/java/com/partola/mytaxi/MyTaxiApplication.java
+++ src/main/java/com/partola/mytaxi/MyTaxiApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyTaxiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MyTaxiApplication.class, args);
         System.out.println("test");
     }
-
 }
