48d52d77b0eb0a905848eb4d2140a6d5656095f8
partolaaa <ivan.partola77@gmail.com>
2023-04-26 12:20:33 +0200

    Initialization

---
+++ src/main/java/com/partola/mytaxi/MyTaxiApplication.java
@@ -0,0 +1,13 @@
+package com.partola.mytaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTaxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTaxiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/partola/mytaxi/MyTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partola.mytaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTaxiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
