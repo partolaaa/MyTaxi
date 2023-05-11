1cfcdc31d4cb2f3723ae08e9f8cf826d40204e5a
Ruslan <krutiporohruslan@gmail.com>
2023-05-11 12:30:26 +0400

    Revert "Added svg background image."
    This reverts commit efe8b28dcb155cfc04b495708c602d5df5002370.

--- src/main/java/mytaxi/controllers/CustomErrorsController.java
+++ src/main/java/mytaxi/controllers/CustomErrorsController.java
@@ -4,7 +4,7 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class CustomErrorsController implements ErrorController {
-    @GetMapping("/error")
+    @RequestMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
